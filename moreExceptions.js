bf2581cd68c58811d1e4b272b3ea6081ebf039a3
Kamlesh Kamble <kamlesh.kamble2.95@gmail.com>
2021-06-07 10:39:34 +0530

    Day 17 - More Exceptions

--- moreExceptions.js
+++ moreExceptions.js
@@ -1,16 +1,27 @@
 console.log("Day 17 - More Expections");
 
 class Calculator {
-  power(n , p) {
-    try {
-      if (n < 0 || p < 0) throw new Error("n and p should be non-negative");
-      else {
-        let x = Math.pow(n, p);
-        return x;
-      }
-    } catch (e){ return e.message}
+  power(n, p) {
+    if (n < 0 || p < 0) {
+      throw "n and p should be non-negative";
+    }
+    return n ** p;
   }
 }
+// power(n, p) {
+//     // Another way to solve the problem
+//     try {
+//     if (n < 0 || p < 0) throw new Error("n and p should be non-negative");
+//     else {
+//         return n**p;
+//         // let x = Math.pow(n, p);
+//         // return x;
+//     }
+//     } catch (e) {
+//     return e.message;
+//     }
+// }
+// }
 
 var myCalculator = new Calculator();
-console.log(myCalculator.power(-3, 5 ));
+console.log(myCalculator.power(-3, 5));
